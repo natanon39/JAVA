3eb8800d49f2dc25b0852c0a246a509fc326a7cb
Natanon Sirapratinrote <31691018+natanon39@users.noreply.github.com>
2021-10-20 11:32:54 +0700

    Update exam1.java

--- exam/exam1.java
+++ exam/exam1.java
@@ -1,6 +1,6 @@
 package exam;
 
-//CREATE PRIME FUNCTION (input = int[], output = prime int[])
+//CREATE PRIME FUNCTION (input = int[], output = prime int[]) and show results
 public class exam1 {
     public static void main(String[] args) {
 
